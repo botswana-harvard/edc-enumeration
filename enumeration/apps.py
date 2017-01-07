617b30b1a286b755eedb2f076c26a8283027a737
erikvw <ew2789@gmail.com>
2017-01-07 23:18:36 +0200

    remove survey messaging from apps

--- enumeration/apps.py
+++ enumeration/apps.py
@@ -1,6 +1,6 @@
 import sys
 
-from django.apps import AppConfig as DjangoAppConfig, apps as django_apps
+from django.apps import AppConfig as DjangoAppConfig
 from django.core.management.color import color_style
 
 style = color_style()
@@ -13,17 +13,4 @@ class AppConfig(DjangoAppConfig):
 
     def ready(self):
         sys.stdout.write('Loading {} ...\n'.format(self.verbose_name))
-        current_surveys = django_apps.get_app_config('survey').current_surveys
-        current_mapper_name = django_apps.get_app_config('edc_map').current_mapper_name
-        if current_surveys.map_area != current_mapper_name:
-            sys.stdout.write(style.ERROR(
-                '\nERROR: Configurations for \'edc_map\' and \'survey\' are incorrect. Expected the \'current_mapper_name\' '
-                '(map_area) to correspond with \'current_survey_label\'. Got {} != {}. See AppConfig for '
-                'edc_map and survey\nYou should also ensure that the \'household\' AppConfig comes after '
-                '\'edc_map\' and \'survey\' in INSTALLED_APPS.\n'.format(
-                    current_mapper_name, current_surveys.map_area)))
-        sys.stdout.write(' * detected current surveys are:\n')
-        for current_survey in current_surveys:
-            sys.stdout.write('   - {}\n'.format(current_survey.label))
-        sys.stdout.write(' * detected current mapper/map_area is \'{}\'\n'.format(current_mapper_name))
         sys.stdout.write(' Done loading {}.\n'.format(self.verbose_name))
