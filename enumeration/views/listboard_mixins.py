6860c0f55b1c91d33fa1d2219ecd78d4056207e2
erikvw <ew2789@gmail.com>
2017-01-24 13:50:47 +0200

    fix filterview for anonymous, fix picking up wrong survey schedule in template

--- enumeration/views/listboard_mixins.py
+++ enumeration/views/listboard_mixins.py
@@ -1,3 +1,4 @@
+from django.apps import apps as django_apps
 from django.db.models import Q
 
 from edc_dashboard.view_mixins import FilteredListViewMixin as BaseFilteredListViewMixin
@@ -35,3 +36,11 @@ class FilteredListViewMixin(BaseFilteredListViewMixin):
         ('household_structure', 'id'),
         ('household_identifier', 'household__household_identifier'),
         ('plot_identifier', 'household__plot__plot_identifier')]
+
+    @property
+    def filtered_queryset(self):
+        plot_identifier = django_apps.get_app_config(
+            'plot').anonymous_plot_identifier
+        return self.filter_model.objects.exclude(
+            household__plot__plot_identifier=plot_identifier).order_by(
+                self.filtered_queryset_ordering)
