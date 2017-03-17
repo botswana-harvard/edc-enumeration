106e00d5cfca9e4b105355136ac9e8dfe09853da
Coulson <ckgathi@bhp.org.bw>
2017-03-17 17:54:40 +0200

    add community filter

--- enumeration/views/listboard_view.py
+++ enumeration/views/listboard_view.py
@@ -1,10 +1,14 @@
+import socket
+
 from django.apps import apps as django_apps
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.conf import settings
 
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_dashboard.views import ListboardView as BaseListboardView
 from edc_dashboard.view_mixins import AppConfigViewMixin
+from edc_map.models import InnerContainer
 
 from household.models import HouseholdStructure
 
@@ -25,6 +29,19 @@ class ListboardView(AppConfigViewMixin, EdcBaseViewMixin, BaseListboardView):
 
     def get_queryset_filter_options(self, request, *args, **kwargs):
         options = super().get_queryset_filter_options(request, *args, **kwargs)
+        map_area = settings.CURRENT_MAP_AREA
+        options.update(
+            {'household__plot__map_area': map_area})
+        device_name = socket.gethostname()
+        plot_identifier_list = []
+        try:
+            plot_identifier_list = InnerContainer.objects.get(
+                device_name=device_name).identifier_labels
+        except InnerContainer.DoesNotExist:
+            plot_identifier_list = []
+        if plot_identifier_list:
+            options.update(
+                {'household__plot__plot_identifier__in': plot_identifier_list})
         if kwargs.get('household_identifier'):
             options.update(
                 {'household__household_identifier': kwargs.get('household_identifier')})
