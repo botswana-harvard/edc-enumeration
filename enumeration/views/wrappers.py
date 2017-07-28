ec82f08f62308709525006c6939727b2dd3579a3
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-07-28 14:53:09 +0200

    fix dashboard

--- enumeration/views/wrappers.py
+++ enumeration/views/wrappers.py
@@ -65,22 +65,22 @@ class HouseholdMemberModelWrapper(BaseHouseholdMemberModelWrapper):
 
         See dashboard_view.get().
         """
-        return self._original_object.editable_in_view
+        return self.object.editable_in_view
 
     @property
     def participation_status(self):
-        return self._original_object.participation_status
+        return self.object.participation_status
 
     @property
     def is_clone_not_updated(self):
-        if (self._original_object.cloned
-                and not self._original_object.clone_updated):
+        if (self.object.cloned
+                and not self.object.clone_updated):
             return True
         return False
 
     @property
     def is_consented(self):
-        return self._original_object.is_consented
+        return self.object.is_consented
 
     @property
     def consent(self):
@@ -88,20 +88,20 @@ class HouseholdMemberModelWrapper(BaseHouseholdMemberModelWrapper):
         """
         # FIXME: self._original_object.consent_object should
         # return a consent object
-        if self._original_object.consent:
-            consent = self._original_object.consent
+        if self.object.consent:
+            consent = self.object.consent
         else:
             try:
-                model = self._original_object.consent_object.model
+                model = self.object.consent_object.model
             except AttributeError:
                 consent = None
             else:
                 consent = model(
-                    subject_identifier=self._original_object.subject_identifier,
+                    subject_identifier=self.object.subject_identifier,
                     consent_identifier=get_uuid(),
-                    household_member=self._original_object,
-                    survey_schedule=self._original_object.survey_schedule_object.field_value,
-                    version=self._original_object.consent_object.version)
+                    household_member=self.object,
+                    survey_schedule=self.object.survey_schedule_object.field_value,
+                    version=self.object.consent_object.version)
                 consent = self.consent_model_wrapper_class(consent)
         if consent:
             consent = self.consent_model_wrapper_class(consent)
@@ -109,18 +109,18 @@ class HouseholdMemberModelWrapper(BaseHouseholdMemberModelWrapper):
 
     def add_extra_attributes_after(self):
         super().add_extra_attributes_after()
-        if self.wrapped_object.id:
-            self.refused = self.wrapped_object.refused
+        if self.object.id:
+            self.refused = self.object.refused
             try:
-                self.dob = self.wrapped_object.enrollmentchecklist.dob
+                self.dob = self.object.enrollmentchecklist.dob
             except ObjectDoesNotExist:
                 self.dob = None
-            self.survival_status = self.wrapped_object.survival_status
-            self.study_resident = self.wrapped_object.study_resident
-            self.get_relation_display = self.wrapped_object.get_relation_display
-            self.get_survival_status_display = self.wrapped_object.get_survival_status_display
+            self.survival_status = self.object.survival_status
+            self.study_resident = self.object.study_resident
+            self.get_relation_display = self.object.get_relation_display
+            self.get_survival_status_display = self.object.get_survival_status_display
             age_helper = AgeHelper(
-                age_in_years=self.wrapped_object.age_in_years)
+                age_in_years=self.object.age_in_years)
             self.is_minor = age_helper.is_minor
             self.is_adult = age_helper.is_adult
             if self.refused:
@@ -145,4 +145,4 @@ class HouseholdStructureWithLogEntryWrapper(
 
     @property
     def members(self):
-        return self.parent._original_object.householdmember_set.all()
+        return self.parent.object.householdmember_set.all()
